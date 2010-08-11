6524d5850975e28d7219441634eaffe0b788ca75
Shawn Tan <shawn.tan@aeste.my>
2010-08-11 22:30:46 +0800

    Working Post.comment++ on Comments/Add operation.

--- controllers/posts_controller.php
+++ controllers/posts_controller.php
@@ -23,195 +23,195 @@ App::import('Sanitize');
 
 class PostsController extends AppController {
 
-	var $name = 'Posts';
-	var $helpers = array ('Form','Html','Text','Ajax','Javascript','Time');
-	var $components = array('RequestHandler');
+  var $name = 'Posts';
+  var $helpers = array ('Form','Html','Text','Ajax','Javascript','Time');
+  var $components = array('RequestHandler');
 
-
-	private function bitly_shorten($url = null) {
-	  $HttpSocket = new HttpSocket();
-	  $bitly = $HttpSocket->get('http://api.bit.ly/v3/shorten', 
-				    array('format' => 'txt',
-					  'login' => 'inorout',
-					  'apiKey' => 'R_11acbfd4019e1d133a8dd8ebb339da03',
+  
+  private function bitly_shorten($url = null) {
+    $HttpSocket = new HttpSocket();
+    $bitly = $HttpSocket->get('http://api.bit.ly/v3/shorten', 
+			      array('format' => 'txt',
+				    'login' => 'inorout',
+				    'apiKey' => 'R_11acbfd4019e1d133a8dd8ebb339da03',
 					  'longUrl' => $url
-					  )
-				    ); 	    
+				    )
+			      ); 	    
+    return trim($bitly);
+  }
+  
+  private function bitly_expand($url = null) {
+    $HttpSocket = new HttpSocket();
+    $bitly = $HttpSocket->get('http://api.bit.ly/v3/expand', 
+			      array('format' => 'txt',
+				    'login' => 'inorout',
+				    'apiKey' => 'R_11acbfd4019e1d133a8dd8ebb339da03',
+				    'shortUrl' => $url
+				    )
+			      ); 	    
 	  return trim($bitly);
-	}
-
-	private function bitly_expand($url = null) {
-	  $HttpSocket = new HttpSocket();
-	  $bitly = $HttpSocket->get('http://api.bit.ly/v3/expand', 
-				    array('format' => 'txt',
-					  'login' => 'inorout',
-					  'apiKey' => 'R_11acbfd4019e1d133a8dd8ebb339da03',
-					  'shortUrl' => $url
-					  )
-				    ); 	    
-	  return trim($bitly);
-	}
-
-	/**
-	 Default - lists all posts
-	 */
-
-	function index() {
-	  $this->set('posts_in', $this->Post->find('all', 
-						array('limit' => 10,
-						      'conditions' => array('Post.ins >= Post.outs'),
-						      'order' => 'Post.id DESC',
-						      )
-						)
-		     );	  
-	  $this->set('posts_out', $this->Post->find('all',
-						array('limit' => 10,
-						      'conditions' => array('Post.outs >= Post.ins'),
-						      'order' => 'Post.id DESC',
-						      )
-						)
-		     );	  
-	  //$this->layout = 'landscape';
-	  $this->set('dump',$this->data);
-	}
-
-	/**
-	 Retrieves and displays a single post.
-	 */
-
-	function view($id = null) {
-	  $this->pageTitle = 'Post #'. $id;
-
-	  // Increment counter once in a session
-	  if (!$this->Session->check('Post.'.$id)) {
-	    $this->Post->updateAll(array('Post.views' => 'Post.views+1'), array('Post.id' => $id));
-	    $this->Session->write('Post.'.$id,'');
-	  }
-
-	  // Extract the post
-	  $this->set('post',$this->Post->read(null,$id));	  
-
-	  // Extract comments
-	  $this->loadModel('Comment');
-	  $this->set('post_comments', 
-		     $this->Comment->find('threaded', 
-					  array('conditions' => array('Comment.post_id' => $id))));	  
-
-	}
-
+  }
+  
+  /**
+   Default - lists all posts
+  */
+  
+  function index() {
+    $this->set('posts_in', $this->Post->find('all', 
+					     array('limit' => 10,
+						   'conditions' => array('Post.ins >= Post.outs'),
+						   'order' => 'Post.id DESC',
+						   )
+					     )
+	       );	  
+    $this->set('posts_out', $this->Post->find('all',
+					      array('limit' => 10,
+						    'conditions' => array('Post.outs >= Post.ins'),
+						    'order' => 'Post.id DESC',
+						    )
+					      )
+	       );	  
+    //$this->layout = 'landscape';
+    $this->set('dump',$this->data);
+  }
+  
+  /**
+   Retrieves and displays a single post.
+  */
+  
+  function view($id = null) {
+    $this->pageTitle = 'Post #'. $id;
+    
+    // Increment counter once in a session
+    if (!$this->Session->check('Post.'.$id)) {
+      $this->Post->updateAll(array('Post.views' => 'Post.views+1'), array('Post.id' => $id));
+      $this->Session->write('Post.'.$id,'');
+    }
+    
+    // Extract the post
+    $this->set('post',$this->Post->read(null,$id));	  
+    
+    // Extract comments
+    $this->loadModel('Comment');
+    $this->set('post_comments', 
+	       $this->Comment->find('threaded', 
+				    array('conditions' => array('Comment.post_id' => $id))));	  
+    
+  }
+  
 	/**
 	 Edit an existing post.
-	 */
-
-	function edit($id = null) {
-	  $this->pageTitle = 'Edit Post #'.$id;
-	  if (empty($this->data)) {
-	    $this->data = $this->Post->read(null, $id);
-	    // Expand the URL using bitly
-	    if (!empty($this->data['Post']['url'])) {	      
-	      $this->data['Post']['url'] = $this->bitly_expand($this->data['Post']['url']);
-	    }
-	  } else {
-	    // Shorten the URL using bitly
-	    if (!empty($this->data['Post']['url'])) {	      
-	      $this->data['Post']['url'] = $this->bitly_shorten($this->data['Post']['url']);
-	    }	    
-	    // save the form
-	    if ($this->Post->save($this->data)) {	      	      
-	      $id = $this->Post->id; // get new ID
-	      $this->Session->setFlash('Post #'. $id .' updated successfully.');
-	      $this->redirect(array('action' => 'view', $id));
-	    }
-	  }
-	}
-
-	/**
-	 Add a new post.
-	 */
-
-	function add() {	
-	  $this->pageTitle = 'Add Post';	 
-	  if (!empty($this->data)) {
-	    // Shorten the URL using bitly
-	    if (!empty($this->data['Post']['url'])) {	      
-	      $this->data['Post']['url'] = $this->bitly_shorten($this->data['Post']['url']);
-	    }	    
-	    // save the form
-	    if ($this->Post->save($this->data)) {	      	      
-	      $id = $this->Post->id; // get new ID
-	      $this->Session->setFlash('Post #'. $id .' added successfully.');
-	      $this->redirect(array('action' => 'view', $id));
-	    }
-	  }
-	}
-
-	/**
-	 Shorten the URL
-	 */
-	function bitly() {
-	  Configure::write('debug', 0); // dont want debug in ajax returned html
-	  if (!empty($this->data['Post']['url'])) {	      
-	    $HttpSocket = new HttpSocket();
-	    $bitly = $HttpSocket->get('http://api.bit.ly/v3/shorten', 
-				      array('format' => 'txt',
-					    'login' => 'inorout',
-					    'apiKey' => 'R_11acbfd4019e1d133a8dd8ebb339da03',
-					    'longUrl' => $this->data['Post']['url']
-					    )
-				      ); 	    
-	    $this->set('bitly', trim($bitly)); // strip whitespace
-	  }	    	  
-	  $this->layout = 'ajax';
-	}
-
-
-	/**
-	 Flag a post as DELETED (AJAX)
-	 */
-	function delete($id = null) {
-	  if ($this->RequestHandler->isAjax()) {
-	    Configure::write('debug', 0); // dont want debug in ajax returned html
-	    // TODO: Check for ACL
-	    $this->Post->updateAll(array('Post.flags' => '-1'), array('Post.id' => $id));
-	    $this->set('result', 'Deleted');
-	    $this->layout = 'ajax';
-	  }
-	}
-
-	/**
-	 Flag a post as FLAGGED (AJAX)
-	 */
-	function flag($id = null) {
-	  if ($this->RequestHandler->isAjax()) {
-	    Configure::write('debug', 0); // dont want debug in ajax returned html
-	    // TODO: Check for ACL
-	    $this->Post->updateAll(array('Post.flags' => 'Post.flags+1'), array('Post.id' => $id));
-	    $this->set('result', 'Flagged');
-	    $this->layout = 'ajax';
-	  }
-	}
-
-	/**
-	 Vote IN/OUT
-	 */
-	function vin($id = null) {
-	  if ($this->RequestHandler->isAjax()) {
-	    Configure::write('debug', 0); // dont want debug in ajax returned html
-	    // TODO: Check for ACL
-	    $this->Post->updateAll(array('Post.ins' => 'Post.ins+1'), array('Post.id' => $id));
-	    //$this->set('result', 'Flagged');
-	    $this->layout = 'ajax';
-	  }	  
-	}
-
-	function vout($id = null) {
-	  if ($this->RequestHandler->isAjax()) {
-	    Configure::write('debug', 0); // dont want debug in ajax returned html
-	    // TODO: Check for ACL
-	    $this->Post->updateAll(array('Post.outs' => 'Post.outs+1'), array('Post.id' => $id));
-	    //$this->set('result', 'Flagged');
-	    $this->layout = 'ajax';
-	  }	  
-	}
+	*/
+  
+  function edit($id = null) {
+    $this->pageTitle = 'Edit Post #'.$id;
+    if (empty($this->data)) {
+      $this->data = $this->Post->read(null, $id);
+      // Expand the URL using bitly
+      if (!empty($this->data['Post']['url'])) {	      
+	$this->data['Post']['url'] = $this->bitly_expand($this->data['Post']['url']);
+      }
+    } else {
+      // Shorten the URL using bitly
+      if (!empty($this->data['Post']['url'])) {	      
+	$this->data['Post']['url'] = $this->bitly_shorten($this->data['Post']['url']);
+      }	    
+      // save the form
+      if ($this->Post->save($this->data)) {	      	      
+	$id = $this->Post->id; // get new ID
+	$this->Session->setFlash('Post #'. $id .' updated successfully.');
+	$this->redirect(array('action' => 'view', $id));
+      }
+    }
+  }
+  
+  /**
+   Add a new post.
+  */
+  
+  function add() {	
+    $this->pageTitle = 'Add Post';	 
+    if (!empty($this->data)) {
+      // Shorten the URL using bitly
+      if (!empty($this->data['Post']['url'])) {	      
+	$this->data['Post']['url'] = $this->bitly_shorten($this->data['Post']['url']);
+      }	    
+      // save the form
+      if ($this->Post->save($this->data)) {	      	      
+	$id = $this->Post->id; // get new ID
+	$this->Session->setFlash('Post #'. $id .' added successfully.');
+	$this->redirect(array('action' => 'view', $id));
+      }
+    }
+  }
+  
+  /**
+   Shorten the URL
+  */
+  function bitly() {
+    Configure::write('debug', 0); // dont want debug in ajax returned html
+    if (!empty($this->data['Post']['url'])) {	      
+      $HttpSocket = new HttpSocket();
+      $bitly = $HttpSocket->get('http://api.bit.ly/v3/shorten', 
+				array('format' => 'txt',
+				      'login' => 'inorout',
+				      'apiKey' => 'R_11acbfd4019e1d133a8dd8ebb339da03',
+				      'longUrl' => $this->data['Post']['url']
+				      )
+				); 	    
+      $this->set('bitly', trim($bitly)); // strip whitespace
+    }	    	  
+    $this->layout = 'ajax';
+  }
+  
+  
+  /**
+   Flag a post as DELETED (AJAX)
+  */
+  function delete($id = null) {
+    if ($this->RequestHandler->isAjax()) {
+      Configure::write('debug', 0); // dont want debug in ajax returned html
+      // TODO: Check for ACL
+      $this->Post->updateAll(array('Post.flags' => '-1'), array('Post.id' => $id));
+      $this->set('result', 'Deleted');
+      $this->layout = 'ajax';
+    }
+  }
+  
+  /**
+   Flag a post as FLAGGED (AJAX)
+  */
+  function flag($id = null) {
+    if ($this->RequestHandler->isAjax()) {
+      Configure::write('debug', 0); // dont want debug in ajax returned html
+      // TODO: Check for ACL
+      $this->Post->updateAll(array('Post.flags' => 'Post.flags+1'), array('Post.id' => $id));
+      $this->set('result', 'Flagged');
+      $this->layout = 'ajax';
+    }
+  }
+  
+  /**
+   Vote IN/OUT
+  */
+  function vin($id = null) {
+    if ($this->RequestHandler->isAjax()) {
+      Configure::write('debug', 0); // dont want debug in ajax returned html
+      // TODO: Check for ACL
+      $this->Post->updateAll(array('Post.ins' => 'Post.ins+1'), array('Post.id' => $id));
+      //$this->set('result', 'Flagged');
+      $this->layout = 'ajax';
+    }	  
+  }
+  
+  function vout($id = null) {
+    if ($this->RequestHandler->isAjax()) {
+      Configure::write('debug', 0); // dont want debug in ajax returned html
+      // TODO: Check for ACL
+      $this->Post->updateAll(array('Post.outs' => 'Post.outs+1'), array('Post.id' => $id));
+      //$this->set('result', 'Flagged');
+      $this->layout = 'ajax';
+    }	  
+  }
 }
 ?>
--- views/layouts/default.ctp
+++ views/layouts/default.ctp
@@ -68,16 +68,15 @@ echo $scripts_for_layout;
   $html->link('',array('controller' => 'pages', 'action' => 'friends'), array('class' => 'btnfren')):
   $html->image('button_friends_clicked.gif');?></li>
   </ul>
-  <div class="clear">&nbsp;</div>
   <div class="grid_12">
   <?php echo $content_for_layout; ?>
   </div>
   <div class="clear">&nbsp;</div>
-  </div>
+  </div><!--container-->
   <div style="background:#eee;">
-  <div class="container_12"><div>
+  <div class="container_12">
   Copyright &copy; 2010 to INOROUT.ORG
-  </div></div>
+  </div>
   </div>
   <pre>
   <?php echo $cakeDebug; ?>
